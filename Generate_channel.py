c4ee8edbc28d1d56611151751c9ca2baeae1857d
RAY_LING <62822498+crazyrayLing@users.noreply.github.com>
2024-04-30 13:12:30 +0800

    Update Generate_channel.py

--- Generate_channel.py
+++ Generate_channel.py
@@ -21,12 +21,12 @@ def generate_rayleigh_channel(sample,Nt,Nr,num_RIS):
     
 def generate_rician_channel(sample,Nt,Nr,num_RIS):
     
-    H2 = torch.randn(sample,num_RIS,Nt, 2).to(device)  # 将张量移动到GPU
+    H2 = torch.randn(sample,num_RIS,Nt, 2).to(device) 
     H2_complex = H2[..., 0] + 1j * H2[..., 1]
     H2_complex = (torch.sqrt(torch.tensor(1/2)) + torch.sqrt(torch.tensor(1/2))* H2_complex / torch.sqrt(torch.tensor(2.0))) 
     H2 = torch.cat((H2_complex.real.unsqueeze(-1), H2_complex.imag.unsqueeze(-1)), dim=-1).view(H2.shape)
 
-    H3 = torch.randn(sample,num_RIS,Nr,2).to(device)  # 将张量移动到GPU
+    H3 = torch.randn(sample,num_RIS,Nr,2).to(device)  
     H3_complex = H3[..., 0] + 1j * H3[..., 1]
     H3_complex = (torch.sqrt(torch.tensor(1/2)) + torch.sqrt(torch.tensor(1/2))* H3_complex / torch.sqrt(torch.tensor(2.0)))
     H3 = torch.cat((H3_complex.real.unsqueeze(-1), H3_complex.imag.unsqueeze(-1)), dim=-1).view(H3.shape)
