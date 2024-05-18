4443a95779e007b9d3a9a0f36518bea532df3408
RAY_LING <62822498+crazyrayLing@users.noreply.github.com>
2024-05-18 23:21:38 +0800

    Update RISmodel.py

--- RISmodel.py
+++ RISmodel.py
@@ -11,7 +11,6 @@ import torch.nn as nn
 class RISModel(nn.Module):
     def __init__(self,input_num,RIS_NUM):
         super(RISModel, self).__init__()
-        
         self.ris_model = nn.Sequential(
             nn.Linear(input_num, 256),
             nn.BatchNorm1d(256),
@@ -23,11 +22,8 @@ class RISModel(nn.Module):
             nn.BatchNorm1d(256),
             nn.Linear(256, RIS_NUM),
             nn.Sigmoid()
-            
         )
-  
     def forward(self, x):
-        
         ris_output = self.ris_model(x)
         return ris_output
     
@@ -67,38 +63,10 @@ class precoding_4x2(nn.Module):
             nn.Linear(SJY, output_num),
             nn.BatchNorm1d(output_num)     
         )
-  
-  
     def forward(self, x):
-        
         output = self.chen_model(x)
         return output
-    
-class precoding_6x2(nn.Module):
-    def __init__(self,input_num,output_num):
-        super(precoding_6x2, self).__init__()
-        
-        # SJY = 256
-        
-        self.chen_model = nn.Sequential(
-            nn.Linear(input_num, 256),
-            # nn.Dropout(0.25),
-            nn.BatchNorm1d(256),
-            nn.ReLU(),
-            nn.Linear(256,384),
-            # nn.Dropout(0.25),
-            nn.BatchNorm1d(384),
-            nn.ReLU(),
-            nn.Linear(384, output_num),
-            # nn.Dropout(0.25),
-            nn.BatchNorm1d(output_num)     
-        )
-  
-  
-    def forward(self, x):
-        
-        output = self.chen_model(x)
-        return output
+
 
 # Instantiate the RISModel
 # model = RISModel()
